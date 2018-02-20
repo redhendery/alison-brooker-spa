1cf41ef6a40f05ddca924ebb8c55aef46b5db895
Red Hendery <redhendery@gmail.com>
2018-02-20 11:49:18 -0800

    update contact details and hours on bookings page

--- src/Bookings.js
+++ src/Bookings.js
@@ -8,6 +8,11 @@ class Bookings extends Component {
         <p>
           We are located in Surrey Hills, Victoria. If you have any questions please <a href="#/contact">contact us</a>.
         </p>
+        <p>Our hours are as follows:</p>
+        <p>Monday: 3pm - 6pm</p>
+        <p>Tuesday: 9am - 12pm</p>
+        <p>Thursday: 4pm - 7pm</p>
+        <p>Friday: 10am - 1pm</p>
         <iframe src='https://ali8un.cliniko.com/bookings?business_id=53165&practitioner_id=79869&appointment_type_id=235132&embedded=true'
         frameborder='0' scrolling='auto' width='100%' height='1000'></iframe>
       </div>
--- src/Contact.js
+++ src/Contact.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
- 
+
 class Contact extends Component {
   render() {
     return (
       <div>
         <h2>Please contact us for enquiries and bookings.</h2>
-        <p>We are located in Surrey Hills, Victoria.</p>
+        <p>We are located at 288a Mont Albert Road in Surrey Hills, Victoria.</p>
         <p><i class="fa fa-phone fa-2x" aria-hidden="true">
           <span itemProp="telephone"><a href="tel:+61398302484">
             03-9830-2484
@@ -20,5 +20,5 @@ class Contact extends Component {
     );
   }
 }
- 
+
 export default Contact;
