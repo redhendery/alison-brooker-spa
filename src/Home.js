8a009e01d425c2a22de025dd3eabb1eb3aa701b2
Red Hendery <redhendery@gmail.com>
2018-02-21 13:00:00 -0800

    add logo to home page, practicioner briefing

--- src/Home.js
+++ src/Home.js
@@ -3,41 +3,21 @@ import React, { Component } from "react";
 class Home extends Component {
   render() {
     return (
-      <div>
-        <h2>Welcome to Quantum Spinal Network</h2>
-        <h4>What is it?</h4>
+      <div id="image_div">
+      <h3>Dr Alison Brooker Chiropractor</h3>
         <p>
-          Network Spinal (NS) is a chiropractic technique that has been developed,
-          refined and taught by its founder Dr Donald Epstein since the early 1980’s.
+          Alison first considered studying Chiropractic in her final year of high school. Instead she ended up
+          studying Podiatry. Some years later it was no longer enough. So, when she asked herself “if there
+          were no ifs, ands, buts or maybes in this world…no barriers…what would I want to be doing?” the
+          unequivocal answer was “be a Network Spinal Chiropractic practitioner”. The following year Alison
+          began the first of 5 years of chiropractic study at RMIT University (Melbourne) to make it happen.
+          And it was totally worth it. Now as the principle Chiropractor at Quantum Network Spinal, Alison
+          delights to go to work each day. Seeing the changes in people spines and lives is an amazing
+          privilege. A keen seminar attendee, Alison is continually refining and updating her skills. Outside of
+          work Alison loves hanging out with her nieces, nephews, great nieces and great nephews (we all
+          LOVE waterslides!), playing table tennis and walking her 2 Labradoodles (Max and Simba).
         </p>
-        <h4>How does it work?</h4>
-        <p>
-          The strategies used in NS care are light specific contacts (finger pressure) to the regions
-          where the meningeal coverings of the spinal cord attach into the spinal column (mainly
-          around the neck and sacrum). These contacts bring awareness to the cortex (brain) and
-          increasing coherence ie higher functioning of the nervous system.
-          NS care is characterised by the development of two “waves”. Firstly, the respiratory wave –
-          as the tension in the spine and surrounding musculature reduces the vertebrae are able to
-          move and rock through greater range of motion with each breath. Secondly the Network
-          Wave - generally begins to develop in Level 2 of care (sometimes earlier). This wave moves
-          through the connective tissue at successively deeper levels with the Levels of care
-          momentarily destabilising the tissue and encouraging a higher level of re-organisation..
-        </p>
-        <h4>Why would I do it?</h4>
-        <p>
-          At Quantum Network Spinal our overall aim is to develop greater coherence and
-          communication within and between the nervous system, musculoskeletal system and
-          emotional system. This is experienced as a higher expression of your authentic self.
-        </p>
-        <h4>Where does pain fit into this picture?</h4>
-        <p>
-          Pain is often the motivator for someone to seek out healthcare. Pain has a purpose. It is
-          meant to interrupt your life. It is a signal that there is not enough energy to self-organise
-          that area. Something is breaking down physically or emotionally. Pain is an invitation to
-          change (behaviour, perception or structure). NS care is all about what you are moving
-          toward, what you want more of. If you no longer had this pain what would you be able to
-          do/think/achieve? Who would you be? What is life calling you to?
-        </p>
+        <img src="/logo.png"></img>
       </div>
     );
   }
